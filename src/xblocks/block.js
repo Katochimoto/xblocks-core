8ffb350a960df275afeaf8b6ba7d57845172ee86
Katochimoto <bufpost@yandex.ru>
2014-04-17 00:38:35 +0400

    change create block

--- src/xblocks/block.js
+++ src/xblocks/block.js
@@ -1,73 +1,29 @@
 (function(xtag, xblocks) {
 
-    xblocks.create = function(blockName) {
-        return new XBlock(blockName);
-    };
+    xblocks.create = function(blockName, options) {
+        options = typeof(options) === 'object' ? options : {};
 
-    function XBlock(blockName) {
-        this._name = blockName;
-    }
-
-    XBlock.prototype.register = function() {
-        var accessors = {};
-        var methods = {};
-
-        Object.keys(this).filter(function(property) {
-            return property.indexOf('_') !== 0;
-
-        }).forEach(function(property) {
-            var descr = Object.getOwnPropertyDescriptor(this, property);
-            var typeProp = typeof(descr.value);
-
-            if (typeProp === 'object') {
-                if (typeof(descr.value.get) === 'function' ||
-                    typeof(descr.value.set) === 'function') {
-
-                    Object.defineProperty(accessors, property, descr);
-                }
-
-            } else if (typeProp === 'function') {
-                Object.defineProperty(methods, property, descr);
-            }
-
-        }, this);
-
-        xtag.register(this._name, {
-            lifecycle: {
-                created: function() {
-                    this.xblock = xblocks.element.create(this);
-                },
-
-                inserted: function() {
-                },
-
-                removed: function() {
-                    this.xblock.destroy();
-                    delete this.xblock;
-                },
-
-                attributeChanged: function(attrName, oldValue, newValue) {
-                    var props = {};
-                    props[attrName] = xblocks.dom.attrs.getRealValue(attrName, newValue);
-                    this.xblock.update(props);
-                }
+        options.lifecycle = {
+            created: function() {
+                this.xblock = xblocks.element.create(this);
             },
 
-            accessors: accessors,
+            inserted: function() {
+            },
 
-            methods: methods,
+            removed: function() {
+                this.xblock.destroy();
+                delete this.xblock;
+            },
 
-            events: {
-                /*
-                click: function(event) {
-                    if (this.hasAttribute('disabled')) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }
-                }
-                */
+            attributeChanged: function(attrName, oldValue, newValue) {
+                var props = {};
+                props[attrName] = xblocks.dom.attrs.getRealValue(attrName, newValue);
+                this.xblock.update(props);
             }
-        });
+        };
+
+        return xtag.register(blockName, options);
     };
 
 }(xtag, xblocks));
