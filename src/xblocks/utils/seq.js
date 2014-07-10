ca5df282f28f77998aa18b2891fe2ba80fb09e37
Katochimoto <bufpost@yandex.ru>
2014-07-11 02:54:39 +0400

    fix clone element

---
+++ src/xblocks/utils/seq.js
@@ -0,0 +1,13 @@
+/* global xblocks */
+/* jshint strict: false */
+
+/**
+ * @param {*} param
+ * @returns {string}
+ */
+xblocks.utils.seq = (function() {
+    var i = 0;
+    return function() {
+        return i++;
+    };
+}());
