c98208164790cc7b453a72fbc80424e22b2bdb94
Katochimoto <bufpost@yandex.ru>
2014-07-16 20:11:47 +0400

    change equals

--- xblocks-core.js
+++ xblocks-core.js
@@ -749,47 +749,78 @@ xblocks.utils.dispatchEvent = function(element, name, params) {
  * @param {*} y
  * @returns {boolean}
  */
+
+xblocks.utils._equal = {
+    array: function(x, y) {
+        if (x === y) {
+            return true;
+        }
+
+        var i = 0;
+        var l = x.length;
+
+	    if (l !== y.length) {
+            return false;
+        }
+
+	    for (; i < l; i++) {
+		    if (!xblocks.utils.equals(x[i], y[i])) {
+                return false;
+            }
+	    }
+
+	    return true;
+    },
+
+    object: function(x, y) {
+        if (x === y) {
+		    return true;
+        }
+
+    	for (var i in x) {
+    		if (y.hasOwnProperty(i)) {
+    			if (!xblocks.utils.equals(x[i], y[i])) {
+                    return false;
+                }
+
+    		} else {
+    			return false;
+    		}
+    	}
+
+    	for (var i in y) {
+    		if (!x.hasOwnProperty(i)) {
+    			return false;
+    		}
+    	}
+
+    	return true;
+    },
+
+    date: function(x, y) {
+        return x.getTime() === y.getTime();
+    },
+
+    regexp: function(x, y) {
+        return x.toString() === y.toString();
+    }
+};
+
 xblocks.utils.equals = function(x, y) {
     if (x === y) {
         return true;
     }
 
-    if (!(x instanceof Object) || !(y instanceof Object)) {
+    var xType = xblocks.utils.type(x);
+    var yType = xblocks.utils.type(y);
+
+    if (xType !== yType) {
         return false;
     }
 
-    if (x.constructor !== y.constructor) {
-        return false;
-    }
-
-    var p;
-    for (p in x) {
-        if (x.hasOwnProperty(p)) {
-            if (!y.hasOwnProperty(p)) {
-                return false;
-            }
-
-            if (x[p] === y[p]) {
-                continue;
-            }
-
-            if (typeof(x[p]) !== 'object') {
-                return false;
-            }
-
-            if (!xblocks.utils.equals(x[p], y[p])) {
-                return false;
-            }
-        }
-    }
-
-    for (p in y) {
-        if (y.hasOwnProperty(p) && !x.hasOwnProperty(p)) {
-            return false;
-        }
-    }
-
-    return true;
+    return xblocks.utils._equal.hasOwnProperty(xType) ?
+        xblocks.utils._equal[xType](x, y) :
+        x == y;
 };
 
 /* xblocks/utils/equals.js end */
