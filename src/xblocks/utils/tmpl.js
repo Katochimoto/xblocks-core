00f52d1710b3e27de46234be5c05317bfd3a9419
Katochimoto <bufpost@yandex.ru>
2014-08-16 13:23:23 +0400

    simple template

---
+++ src/xblocks/utils/tmpl.js
@@ -0,0 +1,30 @@
+/* global xblocks */
+/* jshint strict: false */
+
+(function() {
+    var cache = {};
+
+    /**
+     * @param {string} str
+     * @param {object} param
+     * @returns {string}
+     */
+    xblocks.utils.tmpl = function(str, data) {
+        if (!cache.hasOwnProperty(str)) {
+            cache[str] = new Function('obj',
+               "var p=[],print=function(){p.push.apply(p,arguments);};" +
+               "with(obj){p.push('" +
+               str.replace(/[\r\t\n]/g, " ")
+                   .split("<%").join("\t")
+                   .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+                   .replace(/\t=(.*?)%>/g, "',$1,'")
+                   .split("\t").join("');")
+                   .split("%>").join("p.push('")
+                   .split("\r").join("\\'")
+               + "');}return p.join('');");
+        }
+
+        return data ? cache[str](data) : cache[str];
+    };
+
+}());
