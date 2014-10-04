408c1f40257efa36ec810a6fbcd90349ad211d00
Katochimoto <bufpost@yandex.ru>
2014-10-04 21:36:01 +0400

    debounce

---
+++ src/xblocks/utils/debounce.js
@@ -0,0 +1,18 @@
+/* global xblocks */
+/* jshint strict: false */
+
+xblocks.utils.debounce = function(callback, delay, scope) {
+    delay = delay || 250;
+    var timer = null;
+
+    return function() {
+        var context = scope || this;
+        var args = arguments;
+
+        clearTimeout(timer);
+        
+        timer = setTimeout(function() {
+            callback.apply(context, args);
+        }, delay);
+    };
+};
