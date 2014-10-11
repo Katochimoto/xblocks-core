2f8397b604b331960ea2322d3f832ec816c12580
Katochimoto <bufpost@yandex.ru>
2014-10-11 22:42:08 +0400

    update test

---
+++ test/spec/xblocks.update.js
@@ -0,0 +1,48 @@
+/* global describe, it, expect, xblocks, beforeEach, afterEach */
+/* jshint strict: false */
+
+describe('xblocks - Изменение атрибутов', function() {
+
+    xblocks.view.register('x-element-update', {
+        propTypes: {
+            'bool-attr': React.PropTypes.bool,
+            'number-attr': React.PropTypes.number
+        },
+
+        render: function() {
+            return React.DOM.div({
+                className: (this.props['bool-attr'] ? 'bool' : '')
+            });
+        }
+    });
+
+    xblocks.create('x-element-update');
+
+
+    beforeEach(function() {
+
+    });
+
+    afterEach(function() {
+
+    });
+
+    it('Добавление атрибута вызывает перерисовку', function(done) {
+        var xElement = document.createElement('x-element-update');
+
+        xElement.addEventListener('xb-update', function() {
+            expect(this.getAttribute('bool-attr')).to.be('true');
+            expect(this.querySelector('.bool')).not.to.be(null);
+            done();
+        }, false);
+
+        xElement.addEventListener('xb-created', function() {
+            expect(this.querySelector('.bool')).to.be(null);
+            xElement.setAttribute('bool-attr', 'true');
+        }, false);
+
+        document.body.appendChild(xElement);
+
+    });
+
+});
