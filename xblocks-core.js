347388b6840ad6078e0fa3653a641487e5524e40
Katochimoto <bufpost@yandex.ru>
2015-03-01 12:10:26 +0300

    clean xblocks.react

--- xblocks-core.js
+++ xblocks-core.js
@@ -1067,21 +1067,12 @@ xblocks.event.dispatch = function(element, name, params) {
  */
 xblocks.react = xblocks.react || {};
 
-xblocks.react._idAttributeName = 'data-reactid';
-
-xblocks.react._separator = '.';
-
-var _containersByReactRootID = {};
-
 /**
  * @param {HTMLElement} node
  * @returns {boolean}
  */
 xblocks.react.unmountComponentAtNode = function(node) {
-    var rootId = xblocks.react.getRootID(node);
-
     if (React.unmountComponentAtNode(node)) {
-        delete _containersByReactRootID[ rootId ];
         return true;
     }
 
@@ -1095,72 +1086,7 @@ xblocks.react.unmountComponentAtNode = function(node) {
  * @returns {function}
  */
 xblocks.react.render = function(nextElement, container, callback) {
-    var component = React.render(nextElement, container, callback);
-    _containersByReactRootID[ component._rootNodeID ] = container;
-    return component;
-};
-
-/**
- * @param {String} id
- * @returns {HTMLElement}
- */
-xblocks.react.findContainerForID = function(id) {
-    var rootId = xblocks.react.getReactRootIDFromNodeID(id);
-    return _containersByReactRootID[ rootId ];
-};
-
-/**
- * @param {HTMLElement} node
- * @returns {HTMLElement}
- */
-xblocks.react.findContainerForNode = function(node) {
-    var id = xblocks.react.getID(node);
-    return (id && xblocks.react.findContainerForID(id));
-};
-
-/**
- * @param {HTMLElement} node
- * @returns {?String}
- */
-xblocks.react.getRootID = function(node) {
-    var rootElement = xblocks.react.getRootElementInContainer(node);
-    return (rootElement && xblocks.react.getID(rootElement));
-};
-
-/**
- * @param {HTMLElement} node
- * @returns {?HTMLElement}
- */
-xblocks.react.getRootElementInContainer = function(node) {
-    if (!node) {
-        return null;
-    }
-
-    if (node.nodeType === 9) {
-        return node.documentElement;
-    } else {
-        return node.firstChild;
-    }
-};
-
-/**
- * @param {HTMLElement} node
- * @returns {?String}
- */
-xblocks.react.getID = function(node) {
-    return (node && node.getAttribute && node.getAttribute(xblocks.react._idAttributeName) || '');
-};
-
-/**
- * @param {String} id
- * @returns {?String}
- */
-xblocks.react.getReactRootIDFromNodeID = function(id) {
-    if (id && id.charAt(0) === xblocks.react._separator && id.length > 1) {
-        var index = id.indexOf(xblocks.react._separator, 1);
-        return index > -1 ? id.substr(0, index) : id;
-    }
-    return null;
+    return React.render(nextElement, container, callback);
 };
 
 /* xblocks/react.js end */
